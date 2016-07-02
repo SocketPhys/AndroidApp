efec5f8d14c8ac1fc96ace9954c095c5b64314d9
Adithya Mohan <SocketPhys@gmail.com>
2016-07-02 13:28:13 -0700

    Update VenueMapFragment.java

--- app/src/main/java/org/incubatex/androidapp/VenueMapFragment.java
+++ app/src/main/java/org/incubatex/androidapp/VenueMapFragment.java
@@ -1,10 +1,15 @@
 package org.incubatex.androidapp;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.gson.GsonBuilder;
 
@@ -17,9 +22,20 @@ public class VenueMapFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         cityData = new GsonBuilder().create().fromJson(getArguments().getString("cityData"), CityData.class);
-        String imageFileName = cityData.getCity()+".png"; // this image is stored in the assets/ directory
+        String imageFileName = cityData.getCity(); // this image is stored in the assets/ directory
+        System.out.println(imageFileName);
         // figure out how to read it and display in an ImageView
         final View rootView = inflater.inflate(R.layout.fragment_venue_map, container, false);
+        Resources res = getResources();
+        ImageView imgView=(ImageView) rootView.findViewById(R.id.imageView);
+        String mDrawableName = imageFileName;
+        int resID = res.getIdentifier(mDrawableName , "drawable", rootView.getContext().getPackageName());
+
+        imgView.setImageResource(resID);
         return rootView;
     }
+
+
+
+
 }
